01e2fd26f1875b600454d3227228c08e390c4aff
rodino <rodinofeliciano2002@gmail.com>
2024-01-02 18:55:14 +0000

    iniciando o sistema

---
+++ index.php
@@ -0,0 +1,54 @@
+  <!doctype html>
+<html lang="en">
+ <head>
+   <!-- Required meta tags -->
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   
+   
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="fontawesome/css/all.css">
+  <link rel="stylesheet" type="text/css" href="fontawesome/js/all.js">
+   <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+     
+     <link href="javaScript.js" rel="stylesheet">
+  <link href="#" rel="icon">
+   <script defer src="javaScript.js"> </script>
+
+   <title>Padaria</title>
+
+   
+ </head>
+
+
+
+ <body>
+    <div class=" bg-danger container">
+        <h2 class="container text-light"><i class="fa fa fa-download"></i> vindo ao sistema de padaria</h2>
+    </div>
+  
+
+
+
+
+
+   <!-- Optional JavaScript -->
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+ </body>
+</html>
---
+++ login.php
@@ -0,0 +1,6 @@
+<div class="form-inline container">
+    <form class="form-control container col-lg-6 col-sm-12" action="#" method="post">
+        <input type="text" placeholder="digite o seu nome" class="form-control container text-default">
+        <input type="password" placeholder="digite a sua senha" class="form-control container text-default">
+    </form>
+</div>
